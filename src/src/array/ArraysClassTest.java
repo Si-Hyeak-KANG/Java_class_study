ca5fd9372eaf8d49fdbd55ca11e0db686f1a1d43
Si-Hyeak-KANG <zlcls456@gmail.com>
2022-05-09 23:08:44 +0900

    연구#4_ArraysClass_22.05.09

--- src/src/array/ArraysClassTest.java
+++ src/src/array/ArraysClassTest.java
@@ -8,7 +8,8 @@ public class ArraysClassTest {
 
         int[] arr = new int[]{1,5,2,4,8};
 
-        System.out.println(arr); // [I@3ac3fd8b
-        System.out.println(Arrays.toString(arr)); // [1, 5, 2, 4, 8]
+        Arrays.fill(arr,1,4,7);
+
+        System.out.println(Arrays.toString(arr)); // [1, 7, 7, 7, 8]
     }
 }
