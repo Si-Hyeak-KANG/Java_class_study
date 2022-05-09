c314b7132ebeddeff86202cd4788a95843ea361e
Si-Hyeak-KANG <zlcls456@gmail.com>
2022-05-09 20:32:14 +0900

    연구#4_Array_22.05.09

---
+++ src/src/array/ArraysClassTest.java
@@ -0,0 +1,14 @@
+package array;
+
+import java.util.Arrays;
+
+public class ArraysClassTest {
+
+    public static void main(String[] args) {
+
+        int[] arr = new int[]{1,5,2,4,8};
+
+        System.out.println(arr); // [I@3ac3fd8b
+        System.out.println(Arrays.toString(arr)); // [1, 5, 2, 4, 8]
+    }
+}
--- src/src/array/SystemArray.java
+++ src/src/array/SystemArray.java
@@ -6,13 +6,12 @@ public class SystemArray {
 
     public static void main(String[] args) {
 
-        int[] arr = {1,2,3,4,5};
-        int[] arr2 = {2,2,2,2,2};
+        int[] srcArr = {1,2,3,4,5};
+        int[] destArr = new int[6];
 
-        System.arraycopy(arr, 0 , arr2, 0,5);
+        System.arraycopy(srcArr, 0 , destArr, 3,4);
 
-        System.out.println(Arrays.toString(arr));
-        System.out.println(Arrays.toString(arr2));
+        System.out.println(Arrays.toString(destArr));
 
     }
 }
