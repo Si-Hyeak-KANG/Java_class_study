a527d18cb4300f78d5d5e6a69049e7b30af7a42d
Si-Hyeak-KANG <zlcls456@gmail.com>
2022-06-30 17:02:54 +0900

    no message

---
+++ src/src/test/koplit/daily/Power.java
@@ -0,0 +1,34 @@
+package test.koplit.daily;
+
+public class Power {
+
+    public static void main(String[] args) {
+        long output = power(3, 40);
+        System.out.println(output); // 19334827
+
+        long output2 = power(2, 10);
+        System.out.println(output2); // 1024
+
+        long output3 = power(3, 11);
+        System.out.println(output3); // 177147
+
+        long output4 = power(5, 22);
+        System.out.println(output4); // 70168358
+    }
+
+    public static long power(int base, int exponent) {
+        // TODO:
+
+        if(exponent == 1) {
+            return 1;
+        }
+        wdw213ㅊㅁwdAdwdlfl.;
+        long tmp = power(base, exponent/2);
+
+
+        System.out.println("result = " + result);
+
+        System.out.println("result % 94906249= " + result%94906249);
+        return result % 94906249;
+    }223fd\e2`3`3bbvz
+}
